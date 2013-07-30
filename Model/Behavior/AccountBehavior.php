809ce22592f02bae5753bcc456d2ffef6e815587
Yasuo Harada <slywalker.net@gmail.com>
2013-07-30 14:14:53 +0900

    modified test case

---
+++ Model/Behavior/AccountBehavior.php
@@ -0,0 +1,264 @@
+<?php
+App::uses('Security', 'Utility');
+
+class AccountBehavior extends ModelBehavior {
+
+/**
+ * Setup
+ *
+ * @param array $config
+ * @return void
+ */
+	public function setup(Model $model, $config = array()) {
+		$default = array(
+			'fields' => array(
+				'username' => 'username',
+				'password' => 'password',
+				'passwordToken' => 'password_token',
+				'email' => 'email',
+				'emailVerified' => 'email_verified',
+				'emailToken' => 'email_token',
+				'emailTokenExpires' => 'email_token_expires',
+				'active' => 'active',
+				'lastLogin' => 'last_login',
+				'newPassword' => 'new_password',
+				'confirmPassword' => 'confirm_password'
+			),
+			'emailTokenExpirationTime' => '+1 day',
+			'hashMethod' => 'hash'
+		);
+		$this->settings[$model->alias] = Hash::merge($default, $config);
+	}
+
+/**
+ * Set email token
+ *
+ * @param string $id
+ * @return array On success it returns the user data record
+ */
+	public function setEmailToken(Model $model, $id) {
+		$fields = $this->settings[$model->alias]['fields'];
+		$expirationTime = $this->settings[$model->alias]['emailTokenExpirationTime'];
+
+		$data = array(
+			$model->alias => array(
+				$model->primaryKey => $id,
+				$fields['emailToken'] => $this->generateToken(),
+				$fields['emailTokenExpires'] => $this->emailTokenExpirationTime($expirationTime),
+				$fields['emailVerified'] => 0
+			)
+		);
+		return $model->save($data, array(
+			'validate' => false,
+			'callbacks' => false
+		));
+	}
+
+/**
+ * Verifies a users email by a token that was sent to him via email and flags the user record as active
+ *
+ * @param string $token The token that wa sent to the user
+ * @return array On success it returns the user data record
+ * @throws InvalidArgumentException
+ * @throws RuntimeException
+ */
+	public function verifyEmail(Model $model, $token = null) {
+		$fields = $this->settings[$model->alias]['fields'];
+
+		$user = $model->find('first', array(
+			'contain' => array(),
+			'conditions' => array(
+				$model->alias . '.' . $fields['emailVerified'] => 0,
+				$model->alias . '.' . $fields['emailToken'] => $token
+			),
+			'fields' => array(
+				$model->primaryKey,
+				$fields['email'],
+				$fields['emailTokenExpires']
+			)
+		));
+
+		if (empty($user)) {
+			throw new InvalidArgumentException(__d('base', 'Invalid token, please check the email you were sent, and retry the verification link.'));
+		}
+
+		$expires = strtotime($user[$model->alias][$fields['emailTokenExpires']]);
+		if ($expires < $this->time()) {
+			throw new RuntimeException(__d('base', 'The token has expired.'));
+		}
+
+		$user[$model->alias][$fields['active']] = 1;
+		$user[$model->alias][$fields['emailVerified']] = 1;
+		$user[$model->alias][$fields['emailToken']] = null;
+		$user[$model->alias][$fields['emailTokenExpires']] = null;
+
+		$user = $model->save($user, array(
+			'validate' => false,
+			'callbacks' => false
+		));
+		$model->data = $user;
+		return $user;
+	}
+
+/**
+ * Set email token
+ *
+ * @param string $id
+ * @return array On success it returns the user data record
+ */
+	public function setPasswordToken(Model $model, $id) {
+		$fields = $this->settings[$model->alias]['fields'];
+		$expirationTime = $this->settings[$model->alias]['emailTokenExpirationTime'];
+
+		$data = array(
+			$model->alias => array(
+				$model->primaryKey => $id,
+				$fields['passwordToken'] => $this->generateToken(),
+				$fields['emailTokenExpires'] => $this->emailTokenExpirationTime($expirationTime),
+			)
+		);
+		return $model->save($data, array(
+			'validate' => false,
+			'callbacks' => false
+		));
+	}
+
+/**
+ * Checks the token for a password change
+ *
+ * @param string $token Token
+ * @return mixed False or user data as array
+ */
+	public function checkPasswordToken(Model $model, $token = null) {
+		$fields = $this->settings[$model->alias]['fields'];
+
+		$user = $model->find('first', array(
+			'contain' => array(),
+			'conditions' => array(
+				$model->alias . '.' . $fields['active'] => 1,
+				$model->alias . '.' . $fields['passwordToken'] => $token,
+				$model->alias . '.' . $fields['emailTokenExpires'] . ' >='
+					=> date('Y-m-d H:i:s', $this->time())
+			),
+			'fields' => array(
+				$model->primaryKey
+			)
+		));
+		if (empty($user)) {
+			return false;
+		}
+		return $user;
+	}
+
+/**
+ * Resets the password
+ *
+ * @param array $postData Post data from controller
+ * @return boolean True on success
+ */
+	public function resetPassword(Model $model, $postData = array()) {
+		$fields = $this->settings[$model->alias]['fields'];
+		$hashMethod = $this->settings[$model->alias]['hashMethod'];
+
+		$result = false;
+
+		$tmp = $model->validate;
+		$model->validate = array(
+			$fields['newPassword'] => $tmp[$fields['password']],
+			$fields['confirmPassword'] => array(
+				'required' => array(
+					'rule' => array('compareFields', $fields['newPassword'], $fields['confirmPassword']),
+					'message' => __d('base', 'The passwords are not equal.')
+				)
+			)
+		);
+
+		$model->set($postData);
+		if ($model->validates()) {
+			$model->data[$model->alias][$fields['password']] = $model->{$hashMethod}(
+				$model->data[$model->alias][$fields['newPassword']]
+			);
+			$model->data[$model->alias][$fields['passwordToken']] = null;
+			$model->data[$model->alias][$fields['emailTokenExpires']] = null;
+			$result = $model->save($model->data, array(
+				'validate' => false,
+				'callbacks' => false
+			));
+		}
+
+		$model->validate = $tmp;
+		return $result;
+	}
+
+/**
+ * Updates the last activity field of a user
+ *
+ * @param string $user User ID
+ * @return boolean True on success
+ */
+	public function updateLastLogin(Model $model, $id = null) {
+		$fields = $this->settings[$model->alias]['fields'];
+
+		$model->id = $id;
+		if ($model->exists()) {
+			return $model->saveField($fields['lastLogin'], date('Y-m-d H:i:s', $this->time()));
+		}
+		return false;
+	}
+
+/**
+ * Validation method to compare two fields
+ *
+ * @param mixed $field1 Array or string, if array the first key is used as fieldname
+ * @param string $field2 Second fieldname
+ * @return boolean True on success
+ */
+	public function compareFields(Model $model, $field1, $field2) {
+		if (is_array($field1)) {
+			$field1 = key($field1);
+		}
+
+		if (
+			isset($model->data[$model->alias][$field1]) &&
+			isset($model->data[$model->alias][$field2]) &&
+			$model->data[$model->alias][$field1] == $model->data[$model->alias][$field2]
+		) {
+			return true;
+		}
+		return false;
+	}
+
+/**
+ * Generate token used by the user registration system
+ *
+ * @param int $length Token Length
+ * @return string
+ */
+	public function generateToken($length = 45) {
+		return substr(uniqid(md5(rand())), 0, $length);
+	}
+
+/**
+ * Returns the time the email verification token expires
+ *
+ * @return string
+ */
+	public function emailTokenExpirationTime($expires) {
+		if (is_string($expires)) {
+			$expires = strtotime(date('Y-m-d H:i:s', $this->time()) . ' ' . $expires);
+		} else {
+			$expires = $this->time() + $expires;
+		}
+		return date('Y-m-d H:i:s', $expires);
+	}
+
+/**
+ * Return time
+ *
+ * @return time
+ */
+	public function time() {
+		return time();
+	}
+
+}
---
+++ Test/Case/Model/Behavior/AccountBehaviorTest.php
@@ -0,0 +1,127 @@
+<?php
+App::uses('AccountBehavior', 'Base.Model/Behavior');
+App::uses('Security', 'Utility');
+
+class TestAccountBehavior extends AccountBehavior {
+
+	const TEST_NOW = '2008-03-28 02:45:46';
+
+	public function time() {
+		return strtotime(self::TEST_NOW);
+	}
+
+	public function hash(Model $model, $string, $type = null, $salt = false) {
+		return Security::hash($string, $type, $salt);
+	}
+
+}
+
+class AccountBehaviorTestCase extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array('plugin.base.user');
+
+/**
+ * Start Test callback
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->User = ClassRegistry::init('Base.User');
+		$this->User->Behaviors->attach('Base.TestAccount');
+	}
+
+/**
+ * End a test
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		unset($this->User);
+		ClassRegistry::flush();
+	}
+
+	public function testVerifyEmail() {
+		$result = $this->User->verifyEmail('emailverifiedsuccess');
+		$this->assertEquals($result['User']['email_token'], null);
+		$this->assertEquals($result['User']['email_token_expires'], null);
+		$this->assertEquals($result['User']['email_verified'], 1);
+		$this->assertEquals($result['User']['active'], 1);
+
+		$result = $this->User->data;
+		$this->assertEquals($result['User']['email_token'], null);
+		$this->assertEquals($result['User']['email_token_expires'], null);
+		$this->assertEquals($result['User']['email_verified'], 1);
+		$this->assertEquals($result['User']['active'], 1);
+	}
+
+	public function testVerifyEmailRuntimeException() {
+		$this->setExpectedException('InvalidArgumentException');
+		$this->User->verifyEmail('emailverifiedfalse');
+	}
+
+	public function testVerifyEmailRuntimeException2() {
+		$this->setExpectedException('RuntimeException');
+		$this->User->verifyEmail('emailverifiedfalse2');
+	}
+
+	public function testUpdateLastLogin() {
+		$result = $this->User->updateLastLogin('5');
+		$this->assertEquals($result['User']['last_login'], TestAccountBehavior::TEST_NOW);
+
+		$result = $this->User->updateLastLogin('999');
+		$this->assertFalse($result);
+	}
+
+	public function testCheckPasswordToken() {
+		$result = $this->User->checkPasswordToken('testtoken');
+		$this->assertFalse($result);
+
+		$result = $this->User->checkPasswordToken('testtoken2');
+		$this->assertFalse($result);
+
+		$result = $this->User->checkPasswordToken('testtoken5');
+		$this->assertEquals($result['User']['id'], '515e36a2-5fjj-46b9-8247-584367265f11');
+	}
+
+	public function testResetPassword() {
+		$data = array(
+			'User' => array(
+				'id' => '47ea303a-3cyc-k251-b313-4811c0a800bf',
+				'new_password' => 'newpassword',
+				'confirm_password' => 'newpassword'
+			)
+		);
+		$result = $this->User->resetPassword($data);
+		$this->assertEquals($result['User']['password_token'], null);
+		$this->assertEquals($result['User']['email_token_expires'], null);
+
+		$data = array(
+			'User' => array(
+				'id' => '47ea303a-3cyc-k251-b313-4811c0a800bf',
+				'new_password' => 'newpassword',
+				'confirm_password' => 'typopassword'
+			)
+		);
+		$result = $this->User->resetPassword($data);
+		$this->assertFalse($result);
+	}
+
+	public function testSetEmailToken() {
+		$result = $this->User->setEmailToken('1');
+		$this->assertEquals($result['User']['email_token_expires'], '2008-03-29 02:45:46');
+		$this->assertEquals($result['User']['email_verified'], 0);
+	}
+
+	public function testSetPasswordToken() {
+		$result = $this->User->setPasswordToken('1');
+		$this->assertEquals($result['User']['email_token_expires'], '2008-03-29 02:45:46');
+	}
+
+}
